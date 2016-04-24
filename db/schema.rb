132e4100429fb588b0a48ad9969d7da7d572b9d1
Jorge Laing <ljorge@wharton.upenn.edu>
2016-04-23 21:37:48 -0400

    Changed password column name and added secure password functionality.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420005540) do
+ActiveRecord::Schema.define(version: 20160424012950) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "text"
@@ -40,9 +40,9 @@ ActiveRecord::Schema.define(version: 20160420005540) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
