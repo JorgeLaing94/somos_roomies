f55c0c233eed1b6c46278e943b206eaf312f6a44
Jorge Laing <ljorge@wharton.upenn.edu>
2016-04-27 19:39:55 -0400

    fixed some migration issues, modified views for home page and navigation bar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160424012950) do
+ActiveRecord::Schema.define(version: 20160427230415) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "text"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160424012950) do
     t.string   "address"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
