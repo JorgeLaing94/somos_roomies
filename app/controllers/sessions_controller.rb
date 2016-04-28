fa027bd5fc0fc12d81cfef9b6c8639349c156df0
Jorge Laing <ljorge@wharton.upenn.edu>
2016-04-27 20:19:42 -0400

    added login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(name: params[:name])
+    @user = User.find_by(email: params[:email])
     redirect_to '/login' if @user.nil?
     return if @user.nil?
-    if @user.password == params[:password]
+    if @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to @user
     else
