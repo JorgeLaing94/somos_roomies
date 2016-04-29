428db2e5465f8c34477d2e787e8bda8d1b427412
Jorge Laing <ljorge@wharton.upenn.edu>
2016-04-29 10:31:46 -0400

    bucket

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => 'jorgelaing.cis196',
+      :bucket => 'jorgelaing-cis196',
       :s3_credentials => "#{Rails.root}/config/aws.yml",
     }
   }
